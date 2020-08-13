8801557c49edfa0802cef3690f0f933f4cdca166
cojocariudaniel1 <cojocariudaniel02@gmail.com>
2020-08-13 21:51:19 +0300

    Update: Roulette V1.1

--- roulette/models/roulettes.py
+++ roulette/models/roulettes.py
@@ -3,8 +3,11 @@ class roulette(models.Model):
     _name = 'test13.roulette'
     _description = 'roulette'
 
-    name = fields.Char ('Roulette Name:')
-
+    name = fields.Char ("Roulette Name:")
+    id = fields.Char ('ID')
+    image = fields.Binary(string="Picture", required=True, store=True)
+    image_name = fields.Char("Picture name")
+    data = fields.Date('Date')
 
 
 
