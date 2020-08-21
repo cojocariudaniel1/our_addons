a532a0525fefc4e4f21b8d7acdb375f9d2b8b430
cojocariudaniel1 <cojocariudaniel02@gmail.com>
2020-08-21 18:39:59 +0300

    Update Roullete V1.5: Features: Instalare wizard partial

--- roulette/__manifest__.py
+++ roulette/__manifest__.py
@@ -4,8 +4,7 @@
         'views/clients_view.xml',
         'views/tickets_view.xml',
         'views/results_view.xml',
+        'wizard/results_view.xml',
         'security/ir.model.access.csv'
-
     ]
-"images": ['/static/description/icon.png'],
 }
